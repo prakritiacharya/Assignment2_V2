fbc7b985977478b62a98ca968ac240639b3e6d19
Prakriti Acharya <prakriti@acharya.com>
2025-08-05 15:27:58 -0400

    Implemented View Binding

--- app/src/main/java/ca/georgiancollege/assignment2_v2/MainActivity.java
+++ app/src/main/java/ca/georgiancollege/assignment2_v2/MainActivity.java
@@ -10,15 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        binding = ActivityMainBinding.inflate(getLayoutflater());
+        setContentView(binding.getRoot());
     }
 }
